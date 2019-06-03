a9bdcc68581aee3a8c0866a0368e7e211d2c7b35
Michael <3yebmb@gmail.com>
2019-06-03 15:51:23 +0500

    fill in list

--- app/src/main/java/dev/m13d/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/dev/m13d/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,12 +1,19 @@
 package dev.m13d.trackmysleepquality.sleeptracker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import dev.m13d.trackmysleepquality.R
 import dev.m13d.trackmysleepquality.TextItemViewHolder
 import dev.m13d.trackmysleepquality.database.SleepNight
 
 class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
     var data =  listOf<SleepNight>()
+    set(value) {
+        field = value
+        notifyDataSetChanged()
+    }
 
     override fun getItemCount() = data.size
 
@@ -16,6 +23,8 @@ class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
+        return TextItemViewHolder(view)
     }
 }
